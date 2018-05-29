40d1e00c33d90d3cf98358a5e06d6c695714fd7f
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-29 15:49:47 +0000

    better

--- app.js
+++ app.js
@@ -1,50 +1,35 @@
+
 $(document).ready(function(){
-  
 
-  $('#conductSearch').click(function(){
-    var enteredSearch = $('#enterSearch').val();
+  $("#search").click(function(){
 
+ var searchTerm = $('#searchTerm').val();
 
-    var urlResult = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+enteredSearch+"&format=json&callback=?";
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+    console.log(url);
+      $.ajax({
+        type: "GET",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+          $('#output').html('');
+          for(var i=0;i<data[1].length;i++){
+            $('#output').prepend("<div><div class='btn-primary'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>" );        
+          }    
+   $("#searchTerm").val('');
+        },
+        error: function (errorMessage) {
+         console.log(errorMessage);
+        }
+    });
     
 
-    $.ajax({
-      type:"GET",
-      url:urlResult,
-      async:false,
-      dataType:"json",
-      success:function(wikiData){
-     
-        $('#searchResults').html('');
-        
-
-     for (var i=0; i<wikiData[1].length; i++) {
-        $('#searchResults').prepend("<li><a href="+wikiData[3][i]+">"+wikiData[1][i]+"</a><p>"+wikiData[2][i]+"</p></li>");
-     } 
-
-
-$('#enterSearch').val('');        
-
-
-var list = $('ul');
-var listItems = list.children('li');
-list.append(listItems.get().reverse());
-        
-      },
-      
-      error:function(errorCommunication){
-        alert("There has been an error");
-      }
-      
-      });
-    
-    }); 
-  
- 
-  $('#enterSearch').keypress(function(typing){
-    if (typing.which===13) {
-      $('#conductSearch').click();
-    }
   });
-  
+      $("#searchTerm").keypress(function(e){
+      if(e.which==13){
+        $("#search").click();
+      }
+    });
 });
